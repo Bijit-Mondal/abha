de879d9d60fc030a0f46be052a2e378ab7968750
Bijit Mondal <bijitmondal3011@rediffmail.com>
2025-01-08 22:15:46 +0530

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <ClientOnly>
+    <Tetris
+      class="h-[500px] w-full [mask-image:radial-gradient(450px_circle_at_center,#00C16A,transparent)]"
+      :base="15"
+      square-color="#fcd195"
+    />
+  </ClientOnly>
+</template>
+
+<script lang="ts" setup>
+   import Tetris from '@/components/Tetris.vue'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-64 items-center justify-center">
+    <RainbowButton> Rainbow Button </RainbowButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import RainbowButton from '@/components/Button/RainbowButton.vue'
+</script>
